fcf94624cf5f964e3a1d968fd58d1fbd78d7e539
hwangsujoung <iove951221@gmail.com>
2022-09-21 02:32:02 +0900

    hsj - 작업중

--- resource/js/common.js
+++ resource/js/common.js
@@ -1,10 +1,34 @@
 $(function(){
 
-    // 탭 객체 생성성
+    // 헤더
+    var url = location.href;
+    var pageHref = url.replace(location.protocol+'//'+location.host,'');
+    var menuList = $('.header_wrap .gnb_wrap > ul > li');
+
+    for(var m=0; m<menuList.length; m++){
+        var menu = menuList[m];
+        if($(menu).find('> a').attr('href') == pageHref){
+            $(menu).find('> a').addClass('active');
+        }
+    }
+
+    // 탭 객체 생성
    if($('body').find('[data-tab]').length > 0){
         for(var i=0; i<$('body').find('[data-tab]').length; i++){
             var tab = $('body').find('[data-tab]')[i];
-            new Tab($(tab).find('[data-tab-menu]'), $(tab).find('[data-tab-content]'), 'active');
+            console.log($(tab).attr('data-tab'))
+            if($(tab).attr('data-tab')){
+                var tabId = $(tab).attr('data-tab');
+                new Tab($(tab).find('[data-tab-menu="'+tabId+'"]'), $(tab).find('[data-tab-content="'+tabId+'"]'), 'active');
+            }else{
+                new Tab($(tab).find('[data-tab-menu]'), $(tab).find('[data-tab-content]'), 'active');
+            }
         }
     }
+
+   // 푸터
+   $('.footer_wrap .move_to_top_con').on('click',function(){
+       $("html, body").animate({ scrollTop: 0 });
+       return false;
+   })
 })
