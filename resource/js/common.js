b59341237f4e0fd0fb433be6faa9aa050eded732
hwangsujoung <iove951221@gmail.com>
2022-09-21 20:24:54 +0900

    hsj - 작업중

--- resource/js/common.js
+++ resource/js/common.js
@@ -1,4 +1,16 @@
+function setViewport() {
+    if( $(window).width() <= 480 ) {
+        var ww = ( document.documentElement.clientWidth < window.screen.width ) ? $(window).width() : window.screen.width;
+        var mw = 480;
+        var ratio = ww / mw;
+        $('meta[name="viewport"]').attr( 'content', 'width=' + mw + ', initial-scale=' + ratio + ', user-scalable=no' );
+    } else {
+        $('meta[name="viewport"]').attr( 'content', 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no' );
+    }
+}
 $(function(){
+
+    setViewport();
 
     // 헤더
     var url = location.href;
